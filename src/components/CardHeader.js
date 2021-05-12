d7b1479520380296546adcf8efcf77d61dfb9043
erler <mail@michelerler.com>
2021-05-12 15:29:20 +0200

    back arrow position

--- src/components/CardHeader.js
+++ src/components/CardHeader.js
@@ -1,23 +1,24 @@
 import { Row, Col, Image } from 'react-bootstrap';
 import backArrow from '../images/back-arrow.png';
-export default function CardHeader({ title, backClickHandler }) {
+export default function CardHeader ({ title, backClickHandler }) {
   return (
     <>
       <Row className="align-items-center text-center position-relative py-2">
-        {backClickHandler && (
-          <Image
-            src={backArrow}
-            style={{
-              width: 20,
-              position: 'absolute',
-              left: 10,
-              top: '1rem',
-              zIndex: 5,
-            }}
-            onClick={() => backClickHandler()}
-          />
-        )}
         <Col style={{ marginLeft: 25, marginRight: 25 }}>
+          {backClickHandler && (
+            <Image
+              src={backArrow}
+              style={{
+                width: 20,
+                position: 'absolute',
+                left: 10,
+                top: '1rem',
+                zIndex: 5,
+                cursor: 'pointer'
+              }}
+              onClick={() => backClickHandler()}
+            />
+          )}
           <h2>{title}</h2>
         </Col>
       </Row>
