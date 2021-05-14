0b41771b1df2348f614df01c0e24cfaf2a52488a
erler <mail@michelerler.com>
2021-05-14 15:44:15 +0200

    temporary footer

--- src/pages/gift/claim/ClaimMain.js
+++ src/pages/gift/claim/ClaimMain.js
@@ -5,6 +5,7 @@ import ErrorModal from '../../../components/Error';
 import Processing from '../../../components/Processing';
 import { giftPallet, useSubstrate } from '../../../substrate-lib';
 import Landing from './Landing';
+import Footer from '../Footer';
 import Header from '../Header';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import CreateNewAccount from './new-account/CreateNewAccount';
@@ -161,6 +162,7 @@ export default function ClaimMain() {
           </Col>
         </Row>
       </Container>
+      <Footer />
       <ErrorModal
         show={!!processingError}
         message={processingError}
