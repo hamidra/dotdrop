3c67cf88cba43ea879c74a7a8313d7c7322bbfe2
hra <hamid.alipour@live.com>
2021-05-13 17:07:48 -0700

    add search to extension account dropbox

--- src/components/account/AccountSelector.js
+++ src/components/account/AccountSelector.js
@@ -1,5 +1,62 @@
-import { Dropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Dropdown, Row, Col, Form } from 'react-bootstrap';
 import { stringHelpers } from '../../utils';
+
+const CustomMenu = React.forwardRef(
+  (
+    { children, style, className, 'aria-labelledby': labeledBy, account },
+    ref
+  ) => {
+    const [value, setValue] = useState('');
+    const filteredChildren = React.Children.toArray(children).filter(
+      ({ props: { account } }) => {
+        let str = '';
+        if (account?.address) {
+          str += ` ${account.address} `;
+        }
+        if (account?.meta?.name) {
+          str += ` ${account.meta.name} `;
+        }
+        return !value || str.toLowerCase().includes(value.toLowerCase());
+      }
+    );
+
+    return (
+      <div
+        ref={ref}
+        style={style}
+        className={className}
+        aria-labelledby={labeledBy}>
+        <Form className="mx-2">
+          <Form.Control
+            autoFocus
+            placeholder="Type to filter..."
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+        </Form>
+        <ul className="list-unstyled">{filteredChildren}</ul>
+      </div>
+    );
+  }
+);
+
+const CustomItem = React.forwardRef(({ account, onClick }, ref) => {
+  const nameStr = stringHelpers.truncateMiddle(account?.meta?.name, 10);
+  const addressStr = stringHelpers.truncateMiddle(account?.address, 5);
+  return (
+    <>
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          onClick(e);
+        }}>
+        <div>{nameStr}</div>
+        <div>{addressStr}</div>
+      </div>
+    </>
+  );
+});
 export default function AccounSelector({
   accounts,
   maxStrlength,
@@ -24,16 +81,15 @@ export default function AccounSelector({
           className="w-100 rounded">
           {label}
         </Dropdown.Toggle>
-        <Dropdown.Menu style={{ minWidth: '100%' }}>
+        <Dropdown.Menu style={{ minWidth: '100%' }} as={CustomMenu}>
           {accounts.map((account, idx) => {
-            let accountStr = account?.meta?.name || account.address;
-            accountStr = stringHelpers.truncateMiddle(accountStr, maxStrlength);
             return (
               <Dropdown.Item
                 eventKey={idx}
-                active={account.address === selectedAccount?.address}>
-                {accountStr}
-              </Dropdown.Item>
+                active={account.address === selectedAccount?.address}
+                account={account}
+                as={CustomItem}
+              />
             );
           })}
         </Dropdown.Menu>
