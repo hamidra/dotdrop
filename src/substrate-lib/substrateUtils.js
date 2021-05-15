8e1fe43ba94517da3679b6dbb3d2ae645c29e32e
hra <hamid.alipour@live.com>
2021-05-14 21:08:19 -0700

    trim appending zeros from decimal fractions

--- src/substrate-lib/substrateUtils.js
+++ src/substrate-lib/substrateUtils.js
@@ -1,6 +1,15 @@
 import { decodeAddress, encodeAddress } from '@polkadot/keyring';
 import { hexToU8a, isHex, u8aToHex } from '@polkadot/util';
 import BN from 'bn.js';
+
+const trimEnd = (str, char) => {
+  let i = str.length;
+  while (i > 0 && str.charAt(i - 1) === char) {
+    i -= 1;
+  }
+  return str.substring(0, i);
+};
+
 const utils = {
   validateAddress: (address, ss58Format) => {
     try {
@@ -21,8 +30,15 @@ const utils = {
     const BChainUnit = B10.pow(BChainDecimal);
     const dm = new BN(value).divmod(BChainUnit);
     const wholeStr = dm.div.toString();
-    const decimalStr = dm.mod.toString().padStart(chainDecimal, '0');
-    return `${wholeStr}.${decimalStr.substr(0, decimalPoints)}`;
+    const decimalStr = trimEnd(
+      dm.mod.toString().padStart(chainDecimal, '0'),
+      '0'
+    );
+    let result = wholeStr;
+    if (decimalStr) {
+      result += `.${decimalStr.substr(0, decimalPoints)}`;
+    }
+    return result;
   },
 
   toChainUnit: (value, chainDecimal) => {
@@ -35,7 +51,7 @@ const utils = {
     decimalVal = decimalVal && decimalVal.substr(0, chainDecimal);
     const decimalValLen = decimalVal?.length || 0;
     const BWholeVal = new BN(wholeVal);
-    const BDecimalVal = new BN(decimalVal);
+    const BDecimalVal = new BN(decimalVal || 0);
 
     const BChainWholeVal = BWholeVal.mul(B10.pow(new BN(chainDecimal)));
     const BChainDecimalVal = BDecimalVal.mul(
