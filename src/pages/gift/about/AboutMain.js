9c365591b1257b68411b0a4abeee265f88b48654
erler <mail@michelerler.com>
2021-05-19 15:15:38 +0200

    phosphor icons

--- src/pages/gift/about/AboutMain.js
+++ src/pages/gift/about/AboutMain.js
@@ -8,9 +8,9 @@ export default function AboutMain () {
   return (
     <>
       <Header />
-        <Container className='d-flex flex-column align-items-center'>
+        <Container className='d-flex flex-column align-items-center py-5'>
           <h1 className='py-5' style={{ fontSize: '3.5rem' }}>There's no Better Gift than DOTs</h1>
-          <p className='text-center'>Gif DOTs to your friends and have them join the Polkadot community today. Simply wrap your DOT gift in a uniqe secret and share it with someone close to you!</p>
+          <p className='text-center text-large pb-5'>Gif DOTs to your friends and have them join the Polkadot community today. Simply wrap your DOT gift in a uniqe secret and share it with someone close to you!</p>
           <ProcessExplainerCard />
         </Container>
       <Footer />
--- src/pages/gift/about/ProcessExplainerCard.js
+++ src/pages/gift/about/ProcessExplainerCard.js
@@ -1,13 +1,15 @@
+import { Gift, PaperPlaneTilt, Smiley } from 'phosphor-react';
+
 export default function ProcessExplainerCard () {
   return (
     <div
-      className='d-flex flex-column align-items-center w-100 p-5'
+      className='d-flex flex-column align-items-center w-100'
       style={
           {
             background: 'linear-gradient(74.98deg, #E6007C 0%, #E6007A 36.28%, #7300E6 100%)',
-            /* linear-gradient(74.98deg, #E6007C 0%, #E6007A 36.28%, #7300E6 100%) */
             borderRadius: '1.5rem',
-            color: '#FFF'
+            color: '#FFF',
+            padding: '5rem'
           }
         }
     >
@@ -16,17 +18,22 @@ export default function ProcessExplainerCard () {
       >
         How to Gift DOT
       </h2>
-      <div className='d-flex flex-row flex-grow-1 text-center'>
-        <div>
-            <h3>Create Gift</h3>
+      <div className='row flex-grow-1 text-center'>
+        <div className='col'>
+            <Gift className='mb-4' size={94} />
+            <h3 className='mb-3'>Create Gift</h3>
             <div>Enter a gift amount and generate a unique gift secret.</div>
         </div>
-        <div>
-            <h3>Share Gift with Friends and Family</h3>
+        <div className='col'>
+            <div className='mb-4'>
+                <PaperPlaneTilt size={82} style={{ margin: '6px' }} />
+            </div>
+            <h3 className='mb-3'>Share Gift</h3>
             <div>Send the unique gift secret to a friend or family member.</div>
         </div>
-        <div>
-            <h3>Claim Gift</h3>
+        <div className='col'>
+            <Smiley className='mb-4' size={94} />
+            <h3 className='mb-3'>Claim Gift</h3>
             <div>The recipient creates a new Polkadot account and claims their gift.</div>
         </div>
       </div>
