62ef23e3c55353340eaa36cc992671f0a60ce44c
erler <mail@michelerler.com>
2021-05-20 17:05:13 +0200

    Update PresentAccountPhrase.js

--- src/pages/gift/claim/new-account/PresentAccountPhrase.js
+++ src/pages/gift/claim/new-account/PresentAccountPhrase.js
@@ -14,60 +14,52 @@ export default function PresentAccountPhrase({
   const checkedErrorMessage = 'Please confirm you have saved your account';
   return (
     <>
-      <Card.Body>
+      <Card.Body className='d-flex flex-column'>
         <CardHeader
           title={'Account Seed Phrase'}
           backClickHandler={() => prevStepHandler()}
+          cardText='Write down the following words in order and store them
+          somewhere safe. This seed phrase allows you to recover your
+          account and funds.'
         />
-        <div className="p-4">
-          <Row className="justify-content-center align-items-center">
-            <Col className="d-flex flex-column justify-content-center align-items-center">
-              <p className="text-center">
-                <span className="d-block">
-                  Write down the following words in order and store them
-                  somewhere safe. This seed phrase allows you to recover your
-                  account and funds.
-                </span>
-              </p>
+        <Row className="p-5 justify-content-center align-items-center">
+          {mnemonicWords.map((word, index) => (
+            <Col md={4} key={index}>
+              <div className=" d-flex flex-row border-bottom border-primary">
+                <div className="text-secondary"> {`${index + 1}.`}</div>
+                <div className="text-center m-auto">{`${word}`}</div>
+              </div>
             </Col>
-          </Row>
-          <Row className="p-5 justify-content-center align-items-center">
-            {mnemonicWords.map((word, index) => (
-              <Col md={4} key={index}>
-                <div className=" d-flex flex-row border-bottom border-primary">
-                  <div className="text-secondary"> {`${index + 1}.`}</div>
-                  <div className="text-center m-auto">{`${word}`}</div>
-                </div>
-              </Col>
-            ))}
-          </Row>
-          <Row className="flex-column justify-content-center align-items-center">
-            <Col>
-              <Form.Check
-                type="checkbox"
-                value={checked}
-                label={label}
-                isInvalid={!!checkedError}
-                onChange={(e) => {
-                  setCheckedError('');
-                  setChecked(e.target.checked);
-                }}
-              />
-              {checkedError && (
-                <Form.Text className="text-danger">{checkedError}</Form.Text>
-              )}
-            </Col>
-            <Col className="pt-5 d-flex justify-content-center">
-              <Button
-                onClick={() =>
-                  checked
-                    ? nextStepHandler()
-                    : setCheckedError(checkedErrorMessage)
-                }>
-                Next
-              </Button>
-            </Col>
-          </Row>
+          ))}
+        </Row>
+        <Row className="flex-column justify-content-center align-items-center">
+          <Col>
+            <Form.Check
+              className='ml-2'
+              type="checkbox"
+              value={checked}
+              label={label}
+              isInvalid={!!checkedError}
+              onChange={(e) => {
+                setCheckedError('');
+                setChecked(e.target.checked);
+              }}
+            />
+            {checkedError && (
+              <Form.Text className="text-danger">{checkedError}</Form.Text>
+            )}
+          </Col>
+        </Row>
+        <div className='d-flex flex-grow-1' />
+        <div className="pt-5 d-flex justify-content-center">
+          <Button
+            onClick={() =>
+              checked
+                ? nextStepHandler()
+                : setCheckedError(checkedErrorMessage)
+            }>
+            Next
+          </Button>
         </div>
       </Card.Body>
     </>
