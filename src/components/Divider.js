acee0a25b45b0ef5f060386a966ac8a2b223b457
erler <mail@michelerler.com>
2021-05-20 14:12:37 +0200

    Create Divider.js

---
+++ src/components/Divider.js
@@ -0,0 +1,9 @@
+export default function Divider ({ text }) {
+  return (
+    <div className="d-flex flex-row align-items-center py-5">
+      <div className="d-flex flex-grow-1 border-top"></div>
+      <div className="px-3">{text}</div>
+      <div className="d-flex flex-grow-1 border-top"></div>
+    </div>
+  );
+}
