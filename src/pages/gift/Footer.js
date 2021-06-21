4533b07d28a20a3768f565ebb42290bf47a48cdb
erler <mail@michelerler.com>
2021-06-21 18:36:09 +0200

    responsive

--- src/pages/gift/Footer.js
+++ src/pages/gift/Footer.js
@@ -1,12 +1,12 @@
 export default function Footer ({ selectedAccount }) {
   return (
     <footer
-      className="footer d-flex px-4 py-3"
+      className="footer d-flex flex-column flex-md-row px-4 py-3"
       expand="md"
     >
-        <div className="d-flex flex-row">
-          <div>© {new Date().getFullYear()} All rights reserved.&nbsp;&nbsp;·&nbsp;&nbsp;</div>
-          <div>
+        <div className="d-flex flex-column flex-sm-row">
+          <div className="my-2 my-md-0">© {new Date().getFullYear()} All rights reserved.<span className='d-none d-sm-inline'>&nbsp;&nbsp;·&nbsp;&nbsp;</span></div>
+          <div className="my-2 my-md-0">
             <span>
               <a href="policy" target="_blank">
                 Terms &amp; Conditions
@@ -21,7 +21,7 @@ export default function Footer ({ selectedAccount }) {
           </div>
         </div>
         <div className="footer-grow flex-grow-1" />
-        <div>
+        <div className="my-2 my-md-0">
           <strong>Questions?</strong> <a href='mailto:support@polkadot.network?subject=Gifts'>support@polkadot.network</a>
         </div>
     </footer>
