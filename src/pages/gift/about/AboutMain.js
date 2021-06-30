2e6c31483694779b8fc138ead986ef6129a46ea8
erler <mail@michelerler.com>
2021-06-30 17:02:02 +0200

    padding about page

--- src/pages/gift/about/AboutMain.js
+++ src/pages/gift/about/AboutMain.js
@@ -9,7 +9,7 @@ export default function AboutMain () {
   return (
     <>
       <Header />
-      <Container className="d-flex flex-column align-items-center" style={{ color: '#FFF' }}>
+      <Container className="d-flex flex-column align-items-center py-5" style={{ color: '#FFF' }}>
         <div style={{ paddingBottom: '4rem' }}>
           <h1 className="py-5 text-center" style={{ fontSize: '3.5rem' }}>
             There's No Better Gift than DOTs
