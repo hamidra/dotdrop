adb3536624d3780ba1a67b719bbf171e05f8511a
hamidra <hamid.alipour@gmail.com>
2021-07-01 16:52:49 -0700

    remove overlay center/set border-radius for polkadot

--- src/pages/gift/claim/new-account/PresentAccountPhrase.js
+++ src/pages/gift/claim/new-account/PresentAccountPhrase.js
@@ -4,10 +4,10 @@ import CardHeader from '../../../../components/CardHeader';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Copy, Eye, EyeSlash } from 'phosphor-react';
 
-export default function PresentAccountPhrase ({
+export default function PresentAccountPhrase({
   mnemonicWords,
   nextStepHandler,
-  prevStepHandler
+  prevStepHandler,
 }) {
   const label = 'I have stored my seed phrase in a safe place.';
   const [blurred, setBlurred] = useState(true);
@@ -48,64 +48,64 @@ export default function PresentAccountPhrase ({
             {mnemonicWords.map((word, index) => (
               <Col md={4} key={index}>
                 <div className="seedphrase-item d-flex flex-row">
-                  <div className="seedphrase-item-nr text-secondary"> {`${index + 1}.`}</div>
+                  <div className="seedphrase-item-nr text-secondary">
+                    {`${index + 1}.`}
+                  </div>
                   <div>{`${word}`}</div>
                 </div>
               </Col>
             ))}
           </Row>
-          <Row className="justify-content-start w-100 pt-2" style={{ marginLeft: '-40px' }}>
+          <Row
+            className="justify-content-start w-100 pt-2"
+            style={{ marginLeft: '-40px' }}>
             <div
               className="phrase-toggle text-button p-2"
               style={!blurred ? { display: 'flex' } : { display: 'none' }}
               onClick={() => hideSecret()}>
-              <EyeSlash
-                className="mr-1"
-                size="18"
-              />
+              <EyeSlash className="mr-1" size="18" />
               <span>{'Hide secret words'}</span>
             </div>
             <div
-              className={`phrase-toggle text-center overlay-center p-2 ${
+              className={`phrase-toggle text-center p-2 ${
                 blurred ? 'd-flex' : 'd-none'
               }`}>
-              <Eye
-                className="mr-1"
-                size="18"
-              />
+              <Eye className="mr-1" size="18" />
               <p>{'Reveal seedphrase'}</p>
             </div>
             <CopyToClipboard
-                  text={mnemonicWords.join(' ')}
-                  onCopy={(result) => {
-                    setCopied(result);
-                  }}>
-                  <div className="p-2 mr-2 copy" onClick={(e) => e.stopPropagation()}>
-                  <Copy className='mr-1' size={18} />
-                    <span className='copy'>{copied ? 'Copied' : 'Copy'}</span>
-                  </div>
-              </CopyToClipboard>
+              text={mnemonicWords.join(' ')}
+              onCopy={(result) => {
+                setCopied(result);
+              }}>
+              <div
+                className="p-2 mr-2 copy"
+                onClick={(e) => e.stopPropagation()}>
+                <Copy className="mr-1" size={18} />
+                <span className="copy">{copied ? 'Copied' : 'Copy'}</span>
+              </div>
+            </CopyToClipboard>
           </Row>
         </div>
         <div className="flex-row justify-content-start align-items-center px-2 pt-2">
-            <Form.Check
-              type="checkbox"
-              value={checked}
-              label={label}
-              isInvalid={!!checkedError}
-              onChange={(e) => {
-                setCheckedError('');
-                setChecked(e.target.checked);
-              }}
-            />
-            {checkedError && (
-              <Form.Text className="text-danger">{checkedError}</Form.Text>
-            )}
+          <Form.Check
+            type="checkbox"
+            value={checked}
+            label={label}
+            isInvalid={!!checkedError}
+            onChange={(e) => {
+              setCheckedError('');
+              setChecked(e.target.checked);
+            }}
+          />
+          {checkedError && (
+            <Form.Text className="text-danger">{checkedError}</Form.Text>
+          )}
         </div>
         <div className="d-flex flex-grow-1" />
         <div className="pt-4 d-flex justify-content-center">
           <button
-            className='btn btn-primary'
+            className="btn btn-primary"
             onClick={() =>
               checked ? nextStepHandler() : setCheckedError(checkedErrorMessage)
             }>
