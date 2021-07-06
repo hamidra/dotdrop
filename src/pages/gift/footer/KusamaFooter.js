36f1c98f779ae9d1ef4330658a45e73cd1094585
hamidra <hamid.alipour@gmail.com>
2021-07-06 10:35:56 -0700

    add policy to kusam footer

--- src/pages/gift/footer/KusamaFooter.js
+++ src/pages/gift/footer/KusamaFooter.js
@@ -1,30 +1,28 @@
-export default function Footer ({ selectedAccount }) {
+import { Link } from 'react-router-dom';
+
+export default function Footer({ variant, selectedAccount }) {
   return (
     <footer
-      className="footer d-flex px-4 py-3"
-      variant="dark"
-      expand="md"
-    >
-        <div className="d-flex flex-row">
-          <div>© {new Date().getFullYear()} All rights reserved.&nbsp;·&nbsp;</div>
-          <div>
-            <span>
-              <a href="policy" target="_blank">
-                Terms &amp; Conditions
-              </a>
-            </span>
-            &nbsp;·&nbsp;
-            <span>
-              <a href="policy" target="_blank">
-                Privacy Policy
-              </a>
-            </span>
-          </div>
+      className={`${variant} footer d-flex flex-column flex-md-row px-4 py-3`}
+      expand="md">
+      <div className="d-flex flex-column flex-sm-row">
+        <div className="my-2 my-md-0">
+          © {new Date().getFullYear()} All rights reserved.
+          <span className="d-none d-sm-inline">&nbsp;&nbsp;·&nbsp;&nbsp;</span>
         </div>
-        <div className="footer-grow flex-grow-1" />
-        <div>
-          <strong>Questions?</strong> <a href='mailto:support@kusama.network?subject=NFTs'>support@kusama.network</a>
+        <div className="my-2 my-md-0">
+          <span>
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </span>
         </div>
+      </div>
+      <div className="footer-grow flex-grow-1" />
+      <div>
+        <strong>Questions?</strong>
+        <a href="mailto:support@kusama.network?subject=NFTs">
+          support@kusama.network
+        </a>
+      </div>
     </footer>
   );
 }
