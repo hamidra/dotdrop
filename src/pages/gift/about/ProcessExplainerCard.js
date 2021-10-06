8aa220983824fe26b5dcb84ae6b7ea9dcca0e2bf
erler <mail@michelerler.com>
2021-10-06 15:31:29 +0200

    About page copy

--- src/pages/gift/about/ProcessExplainerCard.js
+++ src/pages/gift/about/ProcessExplainerCard.js
@@ -13,20 +13,20 @@ export default function ProcessExplainerCard ({ giftTheme }) {
         <div className="col">
           <Gift className="mb-4" size={94} />
           <h3 className="mb-3">Create Gift</h3>
-          <div>Enter your gift amount and generate a unique gift secret.</div>
+          <div>Enter a gift amount and generate a unique gift secret.</div>
         </div>
         <div className="col">
           <div className="mb-4">
             <PaperPlaneTilt size={82} style={{ margin: '6px' }} />
           </div>
           <h3 className="mb-3">Share Gift</h3>
-          <div>Send the unique gift secret to a friend or family member.</div>
+          <div>Send the gift secret to a friend or family member.</div>
         </div>
         <div className="col">
           <Smiley className="mb-4" size={94} />
           <h3 className="mb-3">Claim Gift</h3>
           <div>
-            The recipient uses a new or existing {giftTheme.network} account to claim their gift.
+            The recipient creates a new {giftTheme.network} address and claims their gift.
           </div>
         </div>
       </div>
