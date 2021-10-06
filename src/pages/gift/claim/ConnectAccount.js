520ca1cf87fe5a69faa4f1739f385b03286c7ca3
erler <mail@michelerler.com>
2021-10-06 11:57:58 +0200

    Claim Landing Page copy

--- src/pages/gift/claim/ConnectAccount.js
+++ src/pages/gift/claim/ConnectAccount.js
@@ -8,15 +8,15 @@ export default function ConnectAccount ({ setAccountSourceHandler }) {
   return (
     <Card.Body className="d-flex flex-column">
       <CardHeader
-        title="Claim Your Gift"
-        cardText={`To claim your ${giftTheme?.content}, create a new ${giftTheme?.network} account or connect an existing account.`}
+        title={`Claim Your ${giftTheme?.content} Gift`}
+        cardText={`Create a new ${giftTheme?.network} address to store your funds or use an existing account.`}
       />
       <Col className="d-flex flex-column  flex-grow-1 justify-content-center align-items-center">
         <Row className="d-flex flex-column justify-content-center align-items-center pt-2">
           <button
             className="btn btn-primary btn-lg"
             onClick={() => setAccountSourceHandler('NEW')}>
-            {`Create ${giftTheme?.network} Account`}
+            {`Create ${giftTheme?.network} Address`}
           </button>
         </Row>
         <div className="my-4 text-white">Or</div>
