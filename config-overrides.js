cc4fc95d000bbe291f0d60b6dd27e0df0f284628
hamidra <hamid.alipour@gmail.com>
2022-01-19 18:43:04 -0800

    set jest config to transform @polkadot modules

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,18 @@
-module.exports = function override (webpackConfig) {
-  webpackConfig.module.rules.push({
-    test: /\.mjs$/,
-    include: /node_modules/,
-    type: 'javascript/auto'
-  });
+module.exports = {
+  webpack: function (webpackConfig) {
+    webpackConfig.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    });
 
-  return webpackConfig;
+    return webpackConfig;
+  },
+  jest: function (config) {
+    config.transformIgnorePatterns = [
+      '/node_modules/(?!(@polkadot|@babel/runtime/helpers/esm)/)',
+      '^.+\\.module\\.(css|sass|scss)$'
+    ];
+    return config;
+  }
 };
