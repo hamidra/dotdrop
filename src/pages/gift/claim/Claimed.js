b4914d1e028daed2d3eb2477c1f97fbe9e0f240d
hamidra <hamid.alipour@gmail.com>
2022-02-19 08:22:54 -0800

    refactor get asset + add preview images

--- src/pages/gift/claim/Claimed.js
+++ src/pages/gift/claim/Claimed.js
@@ -8,27 +8,40 @@ import { useSubstrate, utils } from '../../../substrate-lib';
 
 export default function Claimed ({ accountAddress, nft }) {
   const { giftTheme } = useSubstrate();
-  const previewUrl = new URL(`collectibles/statemine/${nft.classId}/${nft.instanceId}`, config.NFT_PREVIEW_URL);
+  const previewUrl = new URL(
+    `collectibles/statemine/${nft?.classId}/${nft?.instanceId}`,
+    config.NFT_PREVIEW_URL
+  );
   const cardText = 'Congratulations! You have successfully claimed your NFT.';
   return (
     <>
       <Card.Body className="d-flex flex-column">
-        <CardHeader
-          title={'You broke the wall!'}
-          cardText={cardText}
-        />
-          <Col className="pt-4 d-flex justify-content-center align-items-center">
-            {(nft?.art)
-              ? (<Image style={{ height: 300 }} className="nft" src={nft?.art} />)
-              : (<Image style={{ width: 256 }} src={giftTheme.network === 'Polkadot' ? giftPolkadot : giftKusama} />)}
-          </Col>
+        <CardHeader title={'You broke the wall!'} cardText={cardText} />
+        <Col className="pt-4 d-flex justify-content-center align-items-center">
+          {nft?.image
+            ? (
+            <Image
+              style={{ height: 300 }}
+              className="nft"
+              loading="lazy"
+              src={nft.image}
+            />
+              )
+            : (
+            <Image
+              style={{ width: 256 }}
+              src={giftTheme.network === 'Polkadot' ? giftPolkadot : giftKusama}
+            />
+              )}
+        </Col>
         <div className="flex-grow-1" />
         <Col className="pt-5 d-flex justify-content-center align-items-center">
           <a
             className="btn btn-primary"
             href={previewUrl}
             target="_blank"
-            rel="noreferrer">
+            rel="noreferrer"
+          >
             See your NFT on Singular
           </a>
         </Col>
---
+++ src/substrate-lib/Providers/txUtils.js
@@ -0,0 +1,41 @@
+const getClaimedUniquesAsset = async (classId, instanceId) => {
+  const asset = {
+    classId,
+    instanceId
+  };
+  try {
+    if (classId && instanceId) {
+      let [classMeta, instanceMeta] = await Promise.all([
+        api.query.uniques.classMetadataOf(classId),
+        api.query.uniques.instanceMetadataOf(classId, instanceId)
+      ]);
+
+      classMeta = classMeta?.unwrapOrDefault()?.toHuman();
+      instanceMeta = instanceMeta.unwrapOrDefault()?.toHuman();
+
+      asset.classMetadata = classMeta?.data;
+      asset.instanceMetadata = instanceMeta?.data;
+    }
+  } catch (err) {
+    console.log(`error reading uniques metadata ${err}`);
+  }
+  return asset;
+};
+
+export const getClaimedAssets = async (api, events) => {
+  const claimed = { uniques: [], balances: [], assets: [] };
+  for (const { event } of events) {
+    if (api.events.balances && api.events.balances.Transfer.is(event)) {
+      // parse claimed balances
+      const claimedBalance = event?.data[2]?.toString();
+      claimedBalance && claimed.balances.push(claimedBalance);
+    } else if (api.events.uniques && api.events.uniques.Transferred.is(event)) {
+      // parse claimed uniques
+      const classId = event?.data[0]?.toString();
+      const instanceId = event?.data[1]?.toString();
+      const claimedAsset = await getClaimedUniquesAsset(classId, instanceId);
+      claimedAsset && claimed.uniques.push(claimedAsset);
+    }
+  }
+  return claimed;
+};
