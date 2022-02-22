ba6f3abb1ccf68d69d32131a235c506a09415c7a
hamidra <hamid.alipour@gmail.com>
2022-02-21 17:14:42 -0800

    update about nfts to valentine page

--- src/pages/gift/header/KusamaNFTHeader.js
+++ src/pages/gift/header/KusamaNFTHeader.js
@@ -1,10 +1,10 @@
-import { Dropdown, Nav, Navbar, Media, Row, Col } from 'react-bootstrap';
-import { useHistory, useLocation } from 'react-router-dom';
-import { stringHelpers } from '../../../utils';
-import Identicon from '@polkadot/react-identicon';
-import { DotsThree, ImageSquare } from 'phosphor-react';
-import KusamaLogo from '../../../images/kusama_logo.png';
-import KusamaIcon from '../../../images/kusama_icon.png';
+import { Dropdown, Nav, Navbar, Media, Row, Col } from "react-bootstrap";
+import { useHistory, useLocation } from "react-router-dom";
+import { stringHelpers } from "../../../utils";
+import Identicon from "@polkadot/react-identicon";
+import { DotsThree, ImageSquare } from "phosphor-react";
+import KusamaLogo from "../../../images/kusama_logo.png";
+import KusamaIcon from "../../../images/kusama_icon.png";
 
 const AccountInfoBox = ({ accountAddress }) => {
   const addressStr = stringHelpers.truncateMiddle(accountAddress, 5);
@@ -26,22 +26,34 @@ const AccountInfoBox = ({ accountAddress }) => {
     </Media>
   );
 };
-export default function Header ({ selectedAccount }) {
+export default function Header({ selectedAccount }) {
   const history = useHistory();
   const location = useLocation();
   return (
     <>
       <Navbar
         className="px-3 px-sm-4 py-3"
-        style={{ display: 'grid', gridTemplateColumns: '0px 1fr 0px' }}
-        variant="dark">
+        style={{ display: "grid", gridTemplateColumns: "0px 1fr 0px" }}
+        variant="dark"
+      >
         <Navbar.Brand>
           <a
             href="https://kusama.network"
             target="_blank"
-            rel="noopener noreferrer">
-            <img width={120} className="p-1 d-none d-sm-inline-block" src={KusamaLogo} alt={'Kusama'} />
-            <img width={42} className="p-1 d-sm-none" src={KusamaIcon} alt={'Kusama'} />
+            rel="noopener noreferrer"
+          >
+            <img
+              width={120}
+              className="p-1 d-none d-sm-inline-block"
+              src={KusamaLogo}
+              alt={"Kusama"}
+            />
+            <img
+              width={42}
+              className="p-1 d-sm-none"
+              src={KusamaIcon}
+              alt={"Kusama"}
+            />
           </a>
         </Navbar.Brand>
         <div className="d-flex align-items-center justify-content-end">
@@ -50,8 +62,13 @@ export default function Header ({ selectedAccount }) {
               <div className="d-none d-sm-block w-100 d-sm-none" />
               <div className="d-none d-sm-block flex-grow-0 justify-content-end mr-2 shadow-sm border-0 p-0">
                 <div
-                  style={{ minWidth: '5rem', fontWeight: '400', height: '42px' }}
-                  className="account-box align-items-center text-center d-flex bg-transparent balance-text">
+                  style={{
+                    minWidth: "5rem",
+                    fontWeight: "400",
+                    height: "42px",
+                  }}
+                  className="account-box align-items-center text-center d-flex bg-transparent balance-text"
+                >
                   <AccountInfoBox accountAddress={selectedAccount} />
                 </div>
               </div>
@@ -62,18 +79,21 @@ export default function Header ({ selectedAccount }) {
               className="btn-dropdown p-1 rounded shadow-sm"
               type="button"
               data-toggle="dropdown"
-              id="dropdownMenuButton">
+              id="dropdownMenuButton"
+            >
               <DotsThree size={30} weight="bold" />
             </Dropdown.Toggle>
             <Dropdown.Menu
               aria-labelledby="dropdownMenuButton"
-              className="dropdown-menu-right mt-2 shadow">
+              className="dropdown-menu-right mt-2 shadow"
+            >
               <Dropdown.Item
                 className="px-3"
-                href="https://kusama.network/parachains/#claim-nft"
+                href="https://kusama.network/valentines/"
                 target="_blank"
                 rel="noopener noreferrer"
-                role="link">
+                role="link"
+              >
                 <ImageSquare className="mr-1" size={18} />
                 About NFTs
               </Dropdown.Item>
